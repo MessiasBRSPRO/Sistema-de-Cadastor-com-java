de5491dca6aa55c5d5581afcaa78cf67bdcd0b2c
MessiasBRSPRO <meddisme@gmail.com>
2023-10-01 16:14:29 -0300

    adding a next condition, option of you see the registered personon the list

--- Main.java
+++ Main.java
@@ -5,22 +5,37 @@ public class Main{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
     ArrayList<Person> personList = new ArrayList<>();
-    System.out.println("[1]Register new Person\n[2]See the Person list" +
-            "\n[3]Remove a person\n[4]exit of Application");
-    System.out.println("Whats you will do?");
-    int choice = input.nextInt();
-    switch (choice){
-      case 1:
-        System.out.println("Name of person");
-        String name = input.nextLine();
-        System.out.println("Age of " +name);
-        int age = input.nextInt();
-        System.out.println("Height of " +name);
-        double height = input.nextDouble();;
-        Person person = new Person(name, age, height);
-        personList.add(person);
-        System.out.println("+1 person added");
-    };
 
+    int out = 1;
+
+    while(out != 0){
+      System.out.println("==========================");
+      System.out.println("[1]Register new Person\n[2]See the Person list" +
+              "\n[3]Remove a person\n[4]exit of Application");
+      System.out.println("==========================");
+      System.out.println("Whats you will do?");
+      int choice = input.nextInt();
+      switch (choice){
+        case 1:
+          System.out.println("Name of person");
+          String name = input.next();
+          System.out.println("Age of " +name);
+          int age = input.nextInt();
+          System.out.println("Height of " +name);
+          double height = input.nextDouble();;
+          Person person = new Person(name, age, height);
+          personList.add(person);
+          System.out.println("+1 person added");
+          break;
+        case 2:
+          System.out.println("Person List actually");
+          for (Person persons: personList) {
+            System.out.println("==========================");
+            System.out.println(persons);
+          }
+          break;
+      };
+
+    };
   };
 };
