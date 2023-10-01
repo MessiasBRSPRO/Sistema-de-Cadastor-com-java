1a880dc15126e878925e35557ad69a3965b64e15
MessiasBRSPRO <meddisme@gmail.com>
2023-10-01 15:44:15 -0300

    adding and using the Scanner, adding the Option Menu and establishing the conditions with switch

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main{
   public static void main(String[] args){
-    Person person1 = new Person("Messias", 17, 1.84);
-    System.out.println(person1);
+    Scanner input = new Scanner(System.in);
+    ArrayList<Person> personList = new ArrayList<>();
+    System.out.println("[1]Register new Person\n[2]See the Person list" +
+            "\n[3]Remove a person\n[4]exit of Application");
+    System.out.println("Whats you will do?");
+    int choice = input.nextInt();
+    switch (choice){
+      case 1:
+        System.out.println("Name of person");
+        String name = input.nextLine();
+        System.out.println("Age of " +name);
+        int age = input.nextInt();
+        System.out.println("Height of " +name);
+        double height = input.nextDouble();;
+        Person person = new Person(name, age, height);
+        personList.add(person);
+        System.out.println("+1 person added");
+    };
+
   };
 };
